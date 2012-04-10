09808e658e47180d5145e44dc47cc130d7b46d55
Robert Mitwicki <robert.mitwicki@fxitech.com>
2012-04-10 09:33:37 +0200

    Prepare API to work with sinatra/config_file

--- lib/casserver.rb
+++ lib/casserver.rb
@@ -1,12 +1,15 @@
-module CASServer; end
-
 require 'active_record'
 require 'active_support'
-require 'sinatra/base'
 require 'builder' # for XML views
 require 'logger'
 $LOG = Logger.new(STDOUT)
 
 require 'casserver/server'
 require 'casserver/api'
+require 'casserver/authenticators/base'
 
+# autoload all authenticators
+CASServer::Authenticators.autoload :ActiveDirectoryLDAP, 'lib/casserver/authenticators/active_directory_ldap/'
+CASServer::Authenticators.autoload :LDAP, 'lib/casserver/authenticators/ldap.rb'
+CASServer::Authenticators.autoload :SQL, 'lib/casserver/authenticators/sql.rb'
+CASServer::Authenticators.autoload :Google, 'lib/casserver/authenticators/google.rb'
---
+++ lib/casserver/base.rb
@@ -0,0 +1,20 @@
+require 'sinatra/base'
+require 'sinatra/config_file'
+
+module CASServer 
+  class Base < Sinatra::Base
+    register Sinatra::ConfigFile
+
+    config_file '../../config/config.yml'
+
+    # default configuration
+    set :maximum_unused_login_ticket_lifetime, 5.minutes
+    set :maximum_unused_service_ticket_lifetime, 5.minutes # CAS Protocol Spec, sec. 3.2.1 (recommended expiry time)
+    set :maximum_session_lifetime, 2.days # all tickets are deleted after this period of time
+    set :log, {:file => 'casserver.log', :level => 'DEBUG'}
+    set :uri_path,  ""
+    set :server, 'webrick'
+
+  end
+end
+
